91064b23cca24474aaf6f1d39775d129dfa70fa9
Sarfulah Stewart <fu.stew@yahoo.com>
2019-07-31 09:56:26 -0400

    added sql file

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products (
-    id INT NOT NULL AUTO_INCREMENT,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(45) NULL,
     department_name VARCHAR(45) NULL,
     price DECIMAL(10,2) NULL,
