30d81bab9227f79555332677f680f44ce190fc64
Sarfulah Stewart <fu.stew@yahoo.com>
2019-08-08 15:15:59 -0400

    quantity console logged correctly

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -20,125 +20,170 @@ connection.connect(function (err) {
 function showProducts() {
     connection.query("SELECT * FROM products", function (err, results) {
         for (var i = 0; i < results.length; i++) {
-            console.log("\nItem ID: " + result[i].item_id + " | " + "Product Name: ")
+            console.log("\nItem ID: " + results[i].item_id + " | " + "Product Name: " + results[i].product_name + " | " + "Stock Quantity: " + results[i].stock_quantity)
         }
         productInfo();
     });
+    
 }
 
 function productInfo() {
-    inquirer.prompt([
-        {
-            name: "product",
-            type: "input",
-            message: "What is the ID# of the product you would like to buy?"
-        },
-        {
-            name: "stock_quantity",
-            type: "input",
-            message: "How many units of the product would you like to buy?",
-            // validate: function (value) {
-            //     if (isNaN(value) === false) {
-            //         return true;
-            //     }
-            //     return false;
-            // }
-        }
-    ]).then(function (res) {
-        var merch = res.product_name,
-        var stock_quantity2 = res.stock_quantity;
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                name: "product",
+                type: "input",
+                message: "What is the ID# of the product you would like to buy?"
+            },
+            {
+                name: "stock_quantity",
+                type: "input",
+                message: "How many units of the product would you like to buy?",
+                // validate: function (value) {
+                //     if (isNaN(value) === false) {
+                //         return true;
+                //     }
+                //     return false;
+                // }
+            }
+        ]).then(function (answer) {
+            var chosenItem;
+            for (var i = 0; i < results.length; i++) {
+                if (results[i].product_name === answer.choice) {
+                    chosenItem = results[i];
+                }
+                console.log(answer.product);
+                console.log(answer.stock_quantity);
 
-        conncection.query("SELECT * FROM products WHERE ?", { item_id: merch },
-            function (err, response) {
-                if (err) throw err;
+                connection.query("SELECT * FROM products WHERE ?", { item_id: answer.product }, function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
 
-                if (response.length === 0) {
-                    console.log("Please select valid Product Item ID");
-                    showProducts();
-                } else {
-                    var productResult = response[0];
-                    if (stock_quantity2 <= productResult.stock_quantity) {
-                        console.log("In Stock");
-
-                        var updateInventory = "Update products SET stock_quantity2= " + (productResult.stock_quantity-stock_quantity2) + "WHERE item_id = " + merch;
-
-                        connection.query(updateInventory, function (err, data) {
-                            if (err) throw err;
-                            console.log("Your order was placed successfully! Your total is $" + productResult * stock_quantity2);
-                            console.log("Thank you for shopping with us today!");
-                            continueShopping();
-                        })
-                    } 
-        }
-            })
-    }
-}
-        .then(function (answer) {
-            // when finished prompting, insert a new item into the db with that info
-            connection.query("SELECT * FROM products", function (err, results) {
-                if (err) throw err;
-                // once you have the items, prompt the user for which they'd like to purchase
-                inquirer
-                    .prompt([
-                        {
-                            name: "choice",
-                            type: "input",
-                            choices: function () {
-                                var choiceArray = [];
-                                for (var i = 0; i < results.length; i++) {
-                                    choiceArray.push(results[i].item_name);
+                    if (res[0].stock_quantity >= answer.stock_quantity) {
+                        var newStock = res[0].stock_quantity - answer.stock_quantity;
+                        connection.query(//update the database)
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    price: answer.stock_quantity
+                                },
+                                {
+                                    item_id: chosenItem
                                 }
-                                return choiceArray;
-                            },
-                            message: "What item would you like to purchase?"
-                        }
+                            ],
+                            function (error) {
+                                if (error) throw err;
+                                console.log("Purchase was successful!");
+                                showProducts();
+                            }
+                        );
+                    }
+                    else {
+                        console.log("Insufficient quantity");
+                        showProducts();
+                    }
+
+                });
+
+            }
+
+            // var merch = res.product_name,
+            // var stock_quantity2 = res.stock_quantity;
+
+            // conncection.query("SELECT * FROM products WHERE ?", { item_id: merch },
+            //     function (err, response) {
+            //         if (err) throw err;
+
+            //         if (response.length === 0) {
+            //             console.log("Please select valid Product Item ID");
+            //             showProducts();
+            //         } else {
+            //             var productResult = response[0];
+            //             if (stock_quantity2 <= productResult.stock_quantity) {
+            //                 console.log("In Stock");
+
+            //                 var updateInventory = "Update products SET stock_quantity2= " + (productResult.stock_quantity-stock_quantity2) + "WHERE item_id = " + merch;
+
+            //                 connection.query(updateInventory, function (err, data) {
+            //                     if (err) throw err;
+            //                     console.log("Your order was placed successfully! Your total is $" + productResult * stock_quantity2);
+            //                     console.log("Thank you for shopping with us today!");
+            //                     continueShopping();
+            //                 })
+            //             } 
+            // }
+            //     })
+        })
+    })
+}
+// }
+// .then(function (answer) {
+//             // when finished prompting, insert a new item into the db with that info
+//             connection.query("SELECT * FROM products", function (err, results) {
+//                 if (err) throw err;
+//                 // once you have the items, prompt the user for which they'd like to purchase
+                // inquirer
+                //     .prompt([
+                //         {
+                //             name: "choice",
+                //             type: "input",
+                //             choices: function () {
+                //                 var choiceArray = [];
+                //                 for (var i = 0; i < results.length; i++) {
+                //                     choiceArray.push(results[i].item_name);
+                //                 }
+                //                 return choiceArray;
+                //             },
+                //             message: "What item would you like to purchase?"
+                //         }
                         // {
                         //   name: "purchase",
                         //   type: "input",
                         //   message: "How much would you like to bid?"
                         // }
-                    ])
-                    .then(function (answer) {
-                        // get the information of the chosen item
-                        var chosenItem;
-                        for (var i = 0; i < results.length; i++) {
-                            if (results[i].product_name === answer.choice) {
-                                chosenItem = results[i];
-                            }
-                        }
+                    // ])
+                    // .then(function (answer) {
+                    //     // get the information of the chosen item
+                    //     var chosenItem;
+                    //     for (var i = 0; i < results.length; i++) {
+                    //         if (results[i].product_name === answer.choice) {
+                    //             chosenItem = results[i];
+                    //         }
+                    //     }
 
                         // determine if bid was high enough
-                        if (chosenItem.price < parseInt(answer.price)) {
-                            // bid was high enough, so update db, let the user know, and start over
-                            connection.query(
-                                "UPDATE auctions SET ? WHERE ?",
-                                [
-                                    {
-                                        price: price
-                                    },
-                                    {
-                                        id: chosenItem.id
-                                    }
-                                ],
-                                function (error) {
-                                    if (error) throw err;
-                                    console.log("Purchase was successful!");
-                                    showProducts();
-                                }
-                            );
-                        }
-                        else {
+                        // if (chosenItem.price < parseInt(answer.price)) {
+                        //     // bid was high enough, so update db, let the user know, and start over
+                        //     connection.query(
+                        //         "UPDATE auctions SET ? WHERE ?",
+                        //         [
+                        //             {
+                        //                 price: price
+                        //             },
+                        //             {
+                        //                 id: chosenItem.id
+                        //             }
+                        //         ],
+                        //         function (error) {
+                        //             if (error) throw err;
+                        //             console.log("Purchase was successful!");
+                        //             showProducts();
+                        //         }
+                        //     );
+                        // }
+                        // else {
 
-                            console.log("Insufficient quantity");
-                            showProducts();
-                        }
-                    });
-            })
-        });
-}
+                        //     console.log("Insufficient quantity");
+                        //     showProducts();
+                        // }
+//                     });
+//             })
+//         });
+// }
 
 
-    
+
 
 // function createProducts() {
 //     console.log("Inserting a new product...\n");
